064ef65a51eb8bd1f0696a4e86ee872695d28076
cezarys8 <czarek.skorupski2001@gmail.com>
2024-01-02 20:38:26 +0100

    connect login to spring boot API

--- web-client/app/api/auth/[...nextauth]/options.js
+++ web-client/app/api/auth/[...nextauth]/options.js
@@ -1,5 +1,6 @@
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const options = {
   providers: [
@@ -33,6 +34,38 @@ export const options = {
       },
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_Secret,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "your-username",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        try {
+          const res = await fetch("http://localhost:8080/api/auth/signin", {
+            method: "POST",
+            body: JSON.stringify({
+              username: credentials.username,
+              password: credentials.password,
+            }),
+            headers: { "Content-Type": "application/json" },
+          });
+          const user = await res.json();
+
+          if (res.ok && user) {
+            console.log("CredentialsProvider(User): ", user);
+            return user;
+          }
+        } catch (error) {
+          console.log(error);
+        }
+        return null;
+      },
     }),
   ],
   callbacks: {
